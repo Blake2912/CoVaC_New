c8ca74eb66238919776ecf93f5d0466671e8f088
Jidgnesh <jidgneshthanos@gmail.com>
2021-05-28 00:07:19 +0530

    Added user details and first Dose registration page

--- app/src/main/java/com/example/covactrial/UserDetails.java
+++ app/src/main/java/com/example/covactrial/UserDetails.java
@@ -2,13 +2,23 @@ package com.example.covactrial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserDetails extends AppCompatActivity {
-
+    private Button eNoButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+        eNoButton = findViewById(R.id.NoBtn);
+        eNoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserDetails.this,FirstDoseVaccineSchedule.class));
+            }
+        });
     }
 }
