163605ccf3c2a5a2219e39de15f4f3e3c9b5104e
Varun <kcva18cs@cmrit.ac.in>
2021-06-04 19:39:08 +0530

    Deleted Unwanted Files

--- app/src/main/java/com/example/covactrial/SecondDoseVaccineSchedule.java
+++
@@ -1,9 +0,0 @@
-package com.example.covactrial;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class SecondDoseVaccineSchedule extends AppCompatActivity {
-
-}
