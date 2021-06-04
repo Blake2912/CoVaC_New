d896e39e895d38ac7d9ab70c5bf4b89f49c19e6f
Varun <kcva18cs@cmrit.ac.in>
2021-06-04 20:17:20 +0530

    Second dose page and linked with the prev page

---
+++ app/src/main/java/com/example/covactrial/SecondDosePage.java
@@ -0,0 +1,14 @@
+package com.example.covactrial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondDosePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_dose_page);
+    }
+}
